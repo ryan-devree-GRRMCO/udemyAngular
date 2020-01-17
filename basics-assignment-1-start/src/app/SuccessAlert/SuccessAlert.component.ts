e09e1856b52f556b132ac7ac04a97b0e88d9ee2d
Ryan DeVree <ryan.devree@gmail.com>
2020-01-16 23:20:21 -0500

    added Udemy Angular

---
+++ basics-assignment-1-start/src/app/SuccessAlert/SuccessAlert.component.ts
@@ -0,0 +1,20 @@
+// import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-SuccessAlert',
+  templateUrl: './SuccessAlert.component.html',
+  styleUrls: ['./SuccessAlert.component.css']
+})
+// export class SuccessAlertComponent implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit() {
+//   }
+
+// }
+
+
+export class SuccessAlertComponent{}
